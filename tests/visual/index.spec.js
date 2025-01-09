534d46b6a4ae3a5726fc3cbe82cd171b2782f1ae
Vasyl <vasyl.hryha@epicmax.co>
2025-01-09 12:57:00 +0200

    - feat: setup visual test

---
+++ tests/visual/index.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from '@chromatic-com/playwright'
+
+const routes = [
+  '/dashboard',
+  '/settings',
+  '/preferences',
+  '/users',
+  '/projects',
+  '/payments/payment-methods',
+  '/payments/billing',
+  '/payments/pricing-plans',
+  '/faq',
+  '/auth/login',
+  '/auth/signup',
+  '/auth/recover-password',
+  '/auth/recover-password-email',
+  '/404',
+]
+
+test.describe('Visual Tests for All Routes', () => {
+  routes.forEach((route) => {
+    test(`Visual test for ${route}`, async ({ page }) => {
+      await page.goto(route) // Automatically prepends the baseURL
+      await page.waitForLoadState('networkidle') // Ensure the page is fully loaded
+      await page.waitForTimeout(500) // Optional delay for animations or dynamic content
+      const baselinePath = `${route.replace(/\//g, '_')}.png` // Adjust path based on your setup
+      expect(await page.screenshot()).toMatchSnapshot(baselinePath)
+    })
+  })
+})
