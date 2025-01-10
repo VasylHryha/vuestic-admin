0944d7d2c80be17ce3bc6e15c37e37a8b2683e58
Vasyl <vasyl.hryha@epicmax.co>
2025-01-10 13:02:53 +0200

    - feat: new test

--- tests/e2e/usersPage.spec.js
+++ tests/e2e/usersPage.spec.js
@@ -8,7 +8,8 @@ const MOCKED_USER_DATA = {
 }
 
 test.describe('Users Page Tests', () => {
-  test('Switch between active and inactive users and count results', async ({ page }) => {
+  test('Switch between active and inactive users and count results', async ({ page, baseURL }) => {
+    console.log(`Base URL: ${baseURL}`)
     // Navigate to the users page
     await page.goto('/users')
     await page.waitForLoadState('networkidle') // Ensure the page is fully loaded
